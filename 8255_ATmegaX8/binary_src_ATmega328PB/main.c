35e64e8911516b171e600c9da4cc0a8b8947b460
kitahei88 <kitahei88@yahoo.co.jp>
2025-07-19 19:22:30 +0900

    G1 and 8255 smd

---
+++ 8255_ATmegaX8/binary_src_ATmega328PB/main.c
@@ -0,0 +1,382 @@
+/*
+ * main.c
+ *
+ * Created: 2/8/2024 8:36:16 AM
+ *  Author: takuya
+	CMT signal changer for MZ-80B
+
+	This is a part of the project to make MZ-80B compatible with MZ-2000.
+	Convert and create 8255 inputs and outputs around CMT from MZ-2000 to MZ-80B CMT control using AVR ATmega xx8 seriese(ATmega328P, etc).
+
+	MZ-80BをMZ-2000互換とするためのプロジェクトの一部です
+	AVRのATmega xx8 seriese(ATmega328Pなど)を使って、CMT周りの8255入出力をMZ-2000のものからMZ-80BのCMT制御へ変換、作成します。
+
+	SMDにするため、atmega328pb に変更
+	FUSEなどやや異なる
+ */ 
+
+//#include <xc.h>
+#include <stdbool.h>
+#include <avr/io.h>
+#define F_CPU 8000000UL  // 8 MHz internal clock
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+/* ATmega328PB fuses: Low, High, Extended. */
+// FUSES = {0xE2, 0xD9, 0xFF};		
+// low	CKDIV8	1		clock divide disable
+//		CKOUT	1		clock out disable
+//		SUT		10		high speed rising
+//		CKSEL	0010	internal RC Osc	 :: default
+// high	RSTDIBL	1		PC6 to reset
+//		DWEN	1		Debug wire disable
+//		SPIEN	0		SPI programing enable
+//		WDTON	1		WDT disable
+//		EESAVE	1		EEPROM is erasable
+//		BOOTSZ	00		Boot loader size set to 2048 words :: default
+//		BOOTRST	1		Reset vector set to application section
+// ext	7~4		not use (set 1)
+//      CFD     0       Disable Clock Failure Detection 
+//		BODLEVEL 111	Brownout reset disable	
+
+
+/* ATmega328p fuses: Low, High, Extended. */
+// FUSES = {0xE2, 0xD9, 0xFF};		
+// low	CKDIV8	1		clock divide disable
+//		CKOUT	1		clock out disable
+//		SUT		10		high speed rising
+//		CKSEL	0010	internal RC Osc	 :: default
+// high	RSTDIBL	1		PC6 to reset
+//		DWEN	1		Debug wire disable
+//		SPIEN	0		SPI programing enable
+//		WDTON	1		WDT disable
+//		EESAVE	1		EEPROM is erasable
+//		BOOTSZ	00		Boot loader size set to 2048 words :: default
+//		BOOTRST	1		Reset vector set to application section
+// ext	7~3		not use (set 1)
+//		BODLEVEL 111	Brownout reset disable	
+
+/* for ATmega88 */
+// FUSES = {0xE2, 0xDF, 0xF9}
+
+
+/*
+	definitions
+*/
+
+/*	deprecated.h	*/
+#define cbi(addr,bit) addr &= ~_BV(bit)	/* clear bit	*/
+#define sbi(addr,bit) addr |= _BV(bit)	/* set bit		*/
+#define ibi(addr,bit) addr ^= _BV(bit)	/* invert bit	*/
+
+// CMT signal from PortD input
+#define	STANDY	0	// 00000000
+#define	STOP	1	// 00000001
+#define	AREW	2	// 00000010
+#define PLAY	4	// 00000100
+#define APLAY	16	// 00010000
+#define FF		32	// 00100000
+#define APSS	64	// 01000000
+#define REW		128	// 10000000
+
+// MZ-80B CMT signal to CMT deck
+#define CMT		PORTC
+#define BLK2	PORTC0
+#define BLK1	PORTC1	
+#define PNL		PORTC2
+#define STP		PORTC3
+#define _PLAY_HOLED	PORTC4
+#define RW		PORTC5
+
+#define PPI		PIND
+//  Correspond to 8255 pin 
+//#define PA3		PIND0
+//#define PA5		PIND1
+//#define PA2		PIND2
+//#define PA6		PIND4
+//#define PA1		PIND5
+//#define PA7		PIND6
+//#define PA0		PIND7
+
+#define B2000	PINB0
+#define TAPEEND	PORTB1	// TAPEEND is L for tape is moving. 
+
+/*
+	public variable
+*/
+	volatile unsigned char	action		= STANDY;
+	volatile unsigned char	last_stat	= STANDY;
+	volatile bool 			ipl_stat	= true; 
+
+/*
+	public function
+*/
+	void stop(void);
+	void play(void);
+	void rew(void);
+	void ff(void);
+//	void apss(void);
+//	void aplay(void);
+//	void arew(void);
+	
+static inline void init(void)
+{
+	cli();
+
+	// PE0~3 not use. set input(pull up)
+	PORTE	=	0b00001111;
+	DDRE	=	0b00000000;	
+
+	// PD0~7 set to input (pull up), PD3(TAPECOUNTER) set to INT1 interrupt
+	PORTD	=	0b11111111;
+	DDRD	=	0b00000000;
+	
+	// PC0~5 set to output
+	// PC6 is reset(not use)
+	PORTC	=	0b11000010;
+	DDRC	=	0b11111111;
+		
+	// PB0 input(B/_2000), PB1 output(OC1A TAPEEND)
+	// PB 2,7 reserved(input) PB3~5 to SPI
+	PORTB	=	0b11111111;
+	DDRB	=	0b00000110;
+		
+	MCUCR = 0x00;	
+	// INT1 edge trigger interrupt
+	EICRA = 0x04;	// both edge trigger	
+	EIMSK = 0x00;	// not start
+	
+	TIMSK0 = 0x00;	// 8bits Timer0 not use 
+	TIMSK1 = 0x00;	// 16bits Timer1 not set yet
+	TIMSK2 = 0x00;	// 8bits Timer2 not use
+	PCMSK0 = 0x00;	// PCINT0(0~7) not use
+	PCMSK1 = 0x00;	// PCINT1(8~15) not use
+	PCMSK2 = 0b10100101;	// PCINT2(16~23) set to CMT signal input intterupt, without PD1,3,4,6(AREW, TAPECOUNTER, APLAY, APSS).
+	PCICR	= 0x00;	// PCINT2	is not start yet
+	
+	ACSR	= 0x00; 
+	PRR0		= 0x00;	// full power 
+	PRR1		= 0x00;	// full power 	
+		
+	sei();
+	return;
+}
+
+static inline void interrupt_init(void)
+{	
+//	PCIFR	|= 0x04; 	// reset PCINT2 interrupt demand
+//	PCICR	= 0x04;		// PCINT2	enable
+	// 16bit timer 1 setting for TAPEEND signal, CTC mode
+	TIMSK1	=	0;	
+	TCNT1	=	0;		
+//	ICR1	=	0xFF00;		
+// counter max (for overflow bug)
+	OCR1A	=	(F_CPU)/170;	// 8MHzclock/256*1.5, 1.5 sec. tape stop 
+	TCCR1A	=	0b00000000;		// CTC, OC1A and OC1B to normal pin , top counter is OCR1A 
+	TCCR1B	=	0b00001000;		// timer stop
+	EIMSK = 0x02;				// INT1 interrupt start
+}
+
+/*	
+ 1.5 sec interrupt from timer start  
+ this is TAPEEND status
+*/
+ISR(TIMER1_COMPA_vect)
+{
+	TCCR1B	=	0b00001000;	// timer stop
+	TIMSK1	=	0;
+	sbi(PORTB,TAPEEND);		// TAPEEND set to high until next timer start. 
+	// for AREW and PLAY
+	if ((last_stat == REW) && (~PPI & APLAY) && (ipl_stat == false)) {
+		play();
+		return;
+	} else 	if ((last_stat == PLAY) && (~PPI & AREW) && (ipl_stat == false)) {
+		rew();
+		return;
+	}
+//	stop();
+	last_stat = STOP;
+}
+
+/*
+ almost 2 sec interrupt from timer start
+ this interrupt never occurred. something wrong.(TOP insurance)
+*/
+ISR(TIMER1_OVF_vect)
+{
+	TCCR1B	=	0b00001000;		// timer stop
+	TIMSK1	=	0;		
+	sbi(PORTB,TAPEEND);			// OC1A to high
+}
+
+/*
+Interrupt for TAPECOUNTER operation
+Start timer
+*/
+ISR(INT1_vect)
+{
+	// start timer with interrupt and reset counter
+	TCNT1	= 0;
+	TIMSK1	=	0b00000011;	// OCR1A(TOP) and OVF(0xFFFF) value interrupt
+	TCCR1B	=	0b00001100; // timer start 1/256 clock
+	cbi(PORTB,TAPEEND);		// TAPEEND got to low.
+}
+
+/*
+Intterupt for CMT function call
+*/
+ISR(PCINT2_vect)
+{
+	// multiple functions call never to be asserted at the same time.
+//	PINB = (1 << PINB2) ; //
+	// action |= ~(PPI | ( 1<<PIND3));
+	unsigned char temp_act = ~ (PPI | 0b01011010 ) ; // ignore APSS, APLAY, TAPECOUTNER, AREW
+//	PINB = (1 << PINB2) ; //
+	if (temp_act & 0xff ) {		// get value only when some function is asserted.
+		action = temp_act;
+	}
+	if (ipl_stat) {
+		ipl_stat = false;
+	}
+}
+
+void stop(void)
+{
+//	action &= ~(STOP);
+	action = STANDY;
+	if (last_stat != STOP ) {
+		CMT = (1<<STP | 1<<BLK1 | 1<<_PLAY_HOLED);
+		_delay_ms(15);
+    	CMT = (1<<BLK1 | 1<<_PLAY_HOLED);
+	}
+	last_stat = STOP;
+//	action &= ~(STOP);	
+	return;
+}
+
+void play(void)
+{
+//	action &= ~(PLAY);
+//	action = STANDY;
+	if (last_stat !=PLAY) {
+		if (last_stat != STOP) 	{
+				// reset CMT status
+				CMT = (1<<STP | 1<<BLK1 | 1<<_PLAY_HOLED);
+				_delay_ms(5);
+			}
+		CMT	= (1<<BLK1 | 1<<_PLAY_HOLED);
+		_delay_ms(5);
+		CMT = (1<<RW | 1<<BLK1 | 1<<_PLAY_HOLED);
+		_delay_ms(15);
+		CMT = (1<<BLK1 | 1<<_PLAY_HOLED);
+		_delay_ms(35);
+		CMT = (1<<PNL | 1<< BLK1 | 1<<_PLAY_HOLED);
+		_delay_ms(15);
+		CMT = (1<<BLK1 | 1<<_PLAY_HOLED);
+		last_stat	= PLAY;
+	}
+	action &= ~(PLAY);
+	return;
+}
+
+void ff(void)
+{
+//	action &= ~(FF);
+//	action = STANDY;
+	if (last_stat != STOP){						// for last status is not stop
+		stop();
+	}
+
+	CMT = (1<<BLK1 | 1<<_PLAY_HOLED);			// set Forward state
+	_delay_ms(5);
+	CMT = (1<<RW | 1<<BLK1 | 1<<_PLAY_HOLED);	// latch Forward state
+	_delay_ms(15);
+	CMT = (1<<BLK1 | 1<<_PLAY_HOLED);	
+	_delay_ms(35);
+	CMT = (1<<BLK1 | 1<<BLK2 | 1<<_PLAY_HOLED);	// Reel motor on
+	_delay_ms(15);
+	CMT = (1<<BLK1 | 1<<_PLAY_HOLED);
+	last_stat = FF;
+	action &= ~(FF);	
+	return;
+}
+
+void rew(void)
+{
+//	action &= ~(REW);
+//	action = STANDY;
+	if (last_stat != STOP){					// for last status is not stop
+		stop();
+	}
+
+	CMT = (1<<_PLAY_HOLED); 				// set Rew state
+	_delay_ms(5);
+	CMT = (1<<RW | 1<<_PLAY_HOLED); 		// latch
+	_delay_ms(15);
+	CMT = (1<<_PLAY_HOLED);
+	_delay_ms(15);
+	CMT = (1<<BLK1 | 1<<_PLAY_HOLED);
+	_delay_ms(20);
+	CMT = (1<<BLK1 | 1<<BLK2 | 1<<_PLAY_HOLED); //reel motor on
+	_delay_ms(15);
+	CMT = (1<<BLK1 | 1<<_PLAY_HOLED);
+	last_stat = REW;
+	action &= ~(REW);			
+	return;
+}
+
+/*
+void apss(void)
+{
+	// MZ-80B CMT deck always works as APSS mode.
+	action &= ~(APSS);
+	return;
+}
+*/
+
+/*
+void aplay(void)
+{
+	// aplay check occured in TAPEEND status
+	action &= ~(APLAY);
+	return;
+}
+
+void arew(void)
+{
+	// arew check occured in TAPEEND status
+	action &= ~(AREW);
+	return;
+}
+*/
+
+int main(void)
+{
+	init();
+	// wait for MZ-2000 mode.
+	interrupt_init();					// start all intterrupt
+    sbi(CMT,_PLAY_HOLED);
+	while(bit_is_set(PINB,B2000)) 	{
+//		sbi(PCIFR , PCIF2) ; 
+		action	= STANDY ;
+	}
+	// start 
+	PCIFR	|= 0x04; 	// reset PCINT2 interrupt demand
+	PCICR	= 0x04;		// PCINT2	enable
+	last_stat = REW;
+    CMT = (1<<BLK1 | 1<<_PLAY_HOLED);	// default IPL CMT status
+
+//	interrupt_init();					// start all intterrupt
+	// signal change function enable
+    while(1)
+    {
+		if (action & STOP)	stop();
+		if (action & PLAY)	play();
+		if (action & FF)	ff();
+		if (action & REW)	rew();
+//		if (action & APSS)	apss();
+//		if (action & APLAY)	aplay();
+//		if (action & AREW)	arew();
+		while (action == STANDY) ;
+    }	
+}
---
+++ MZG1_XC9536XL/main.v
@@ -0,0 +1,170 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:03:45 05/11/2025 
+// Design Name: 
+// Module Name:    main 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+`default_nettype none
+/*  
+    Addressdecoder for MZ-80B to MZ-2000  
+    topmodule
+    2024.06.12 kitahei88, Takuya Fukuda
+    ATF15XX series
+    4MHz  1clk = 250ns
+*/
+
+ module MZG1
+(
+    input CLK,          // 4MHz clk from MZ-80B
+ //   input RST,          // Reset signal from MZ-80B
+    input DSPAD, DSP, nMRAM, // nMRAM is L to All ram mode, H is IPL ROM , nMRAM means Reset signal. 
+    input [3:0]AD,      // A15 to A12   
+    input nEXWAIT,
+    input nRFSH, nMREQ, nIORQ, nRD, nM1,    // for CPU signal
+    input Bn2000,
+	 input BLANK,
+//	 input BLANK_2K, BLANK_B,
+	 input delayLCSW,		// external delayed LCSW AND fast LCSW for BUFGM
+	 
+    output nRAS0, nRAS1, nRAS2, nRAS3, fastLCSW,   // nRAS0 to nRAS3. LCSW is MUX, L is row address and nCAS to H , H is column address and nCAS to L, need delay circuit.
+    output LCSW, // real LCSW , L is Row address and H is Column address
+	 output nCSED, nCSDD,    // for MZ-80B, MZ-2000 , GRAM, CGRAM select
+    output nROMCS,
+	 output BUFGM, BUFG0,  // BUFGM is memory,L RD(DRAM Dout on Data bus) H WD(DRAM Dout off Data bus).  BUFG0 is CPU L=RD,H=WD.
+    output nWAIT
+//    output tp1
+);
+//    wire nGRAM_stat ;
+//    assign nGRAM_stat = nCSED & nCSDD ;
+//    assign tp1 = nGRAM_stat;
+	
+//	 wire BLANK;
+//	 assign BLANK = (Bn2000 ? BLANK_B : BLANK_2K);
+	 
+    reg [2:0]cnt; // 3bit  clock counter 0~7
+    always @(posedge CLK or posedge nMREQ) begin
+        if (nMREQ) begin
+            cnt <= 3'b0;
+        end else begin
+            cnt <= cnt + 3'b001;
+		end
+    end
+
+    // nRAS0~3
+    // MZ-80B access 4116 DRAM, READ CYCLE TIMING, WRITE CYCLE TIMING and nRAS only refresh timing mode.
+    // READ is made from nMREQ and nRD, Write is made from nMREQ and ~nRD
+    // refresh made from nRFSH
+    // not affect Bn2000
+    assign nRAS0 = ( nMREQ | 
+									( ( nMRAM ? ~(AD[3:2] == 2'b10)     // IPL status , AD 0x8000~0xBFFF  
+                                     : ~(AD[3:2] == 2'b00) )   // ALL RAM status , AD 0x0000~0x3FFF
+									  & nRFSH ) 								// refresh status 
+									);                        			                       
+ 
+    assign nRAS1 = ( nMREQ | 
+									( ( nMRAM ? ~(AD[3:2] == 2'b11)     // IPL status , AD 0xC000~0xFFFF  
+                                     : ~(AD[3:2] == 2'b01) )   // ALL RAM status , AD 0x4000~0x7FFF
+									  & nRFSH )  								// refresh status  
+									);                                           
+
+    assign nRAS2 = ( nMREQ | 
+									( ( nMRAM ? 1      						// IPL status , not assert  
+                                     : ~(AD[3:2] == 2'b10) ) 	// ALL RAM status , AD 0x8000~0xBFFF
+									  & nRFSH ) 									// refresh status 
+									);                                            
+
+    assign nRAS3 = ( nMREQ | 
+									( ( nMRAM ? 1    							 // IPL status , not assert  
+                                     : ~(AD[3:2] == 2'b11) ) 	 // ALL RAM status , AD 0xC000~0xFFFF
+									  & nRFSH )  									 // refresh status 
+									);                                                                        
+
+    // LCSW means MUX ROW and COLOUM
+    // assert with RAS but not RFSH status
+    // not affect Bn2000 , need external delay circuit(50ns)
+    assign fastLCSW = (nRFSH ? ~(nRAS0 & nRAS1 & nRAS2 & nRAS3) : 1'b0);
+	 assign LCSW = fastLCSW & delayLCSW;
+	 
+    // nWAIT
+    // CSED and CSDD while BLANK
+    // nEXWAIT can assert nWAIT
+    // IPL ROM access
+    // until wait , no refresh occure, wait is stop z80 cpu at T2 state.
+    assign nWAIT =  nEXWAIT // wait from external board
+                    & ( BLANK | ( csed_req(DSPAD, DSP, nMREQ, nRFSH, AD, Bn2000) & csdd_req(DSPAD, DSP, nMREQ, nRFSH, AD, Bn2000) ) )  // gram access wait until blank
+                    & ( nROMCS | ~(cnt <= 3'b001));     // during IPL ROM access, need 1clk wait.
+
+    // nCSED, nCSDD
+    // assert at DSPAD and DSP and BLANK status
+    assign nCSED = ~BLANK | csed_req(DSPAD, DSP, nMREQ, nRFSH, AD, Bn2000);
+    function csed_req(
+        input dspad,
+		  input dsp, 
+		  input nmreq, 
+		  input nrfsh, 
+		  input [3:0]ad, 
+		  input bn2   
+    );
+        begin
+        if (bn2 == 1'b1)  // MZ-80b
+            begin
+             csed_req = ~( dsp & !nmreq & nrfsh & (dspad ? ( (ad == 4'h6) || (ad == 4'h7) ) 
+																			: ( (ad == 4'hE) || (ad == 4'hF) ) ) );
+            end else begin // MZ-2000
+             csed_req = ~( dsp & !nmreq & nrfsh & !dspad & (ad >= 4'hC) );
+            end
+        
+        end
+    endfunction
+
+    assign nCSDD = ~BLANK | csdd_req(DSPAD, DSP, nMREQ, nRFSH, AD, Bn2000);
+    function csdd_req(
+        input dspad, 
+		  input dsp, 
+		  input nmreq, 
+		  input nrfsh, 
+		  input [3:0]ad, 
+		  input bn2   
+    );
+        begin
+        if (bn2 == 1'b1)  // MZ-80b
+            begin
+             csdd_req = ~(dsp & !nmreq & nrfsh & (dspad ? (ad == 4'h5) 
+                                                    : (ad == 4'hD) ) );             
+            end else begin // MZ-2000
+             csdd_req = ~( dsp & !nmreq & nrfsh & dspad & (ad == 4'hD) );
+            end
+        
+        end
+    endfunction
+
+
+    // nROMCS
+    // IPL status set acitive
+    // if nROMCS is asserted, address access change 0x8000~0xFFFF to nRAS0 nRAS1(0x0000~0x7FFF)
+    // not affect Bn2000
+    assign nROMCS = ~(nMRAM & !nMREQ & !nRD & nRFSH & (AD[3] == 1'b0) );
+	 
+    // BUFGM, BUFG0
+    // BUFGM is memory read or not, IOR status is not assert
+    // BUFG0 is CPU RD or WD status, and intterrupt status is RD
+    // not affect Bn2000
+    assign BUFG0 = nRD & (nM1 | nIORQ);
+    assign BUFGM = nRD | ~nIORQ;
+
+endmodule
+
